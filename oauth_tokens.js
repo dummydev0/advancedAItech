56b128e782ce8fdb1eff1a24df785dee73ababe3
dummydev0 <83955309+dummydev0@users.noreply.github.com>
2024-10-22 12:24:01 +0300

    Create oauth_tokens.js

---
+++ oauth_tokens.js
@@ -0,0 +1,10 @@
+// OAuth Tokens Mock File, THIS IS A TEST FILE
+domain=socradar.com
+const oauthTokens = {
+    access_token: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjdlN2JlIn1.mockOAUTH.token.eyJ1c2VybmFtZSI6InVzZXIxIiwic2NvcGUiOiJyZWFk",
+    refresh_token: "mockRefreshToken1234567890abcdef",
+    token_type: "Bearer",
+    expires_in: 3600
+};
+
+module.exports = oauthTokens;
