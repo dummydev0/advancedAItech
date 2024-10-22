8277ac9eac8180ec4656e1beef370144a9669dcc
dummydev0 <83955309+dummydev0@users.noreply.github.com>
2024-10-22 22:05:23 +0300

    Update oauth_tokens.js

--- oauth_tokens.js
+++ oauth_tokens.js
@@ -1,5 +1,5 @@
 // OAuth Tokens Mock File, THIS IS A TEST FILE
-domain=socradar.com
+auth_url="https://socradar.com"
 const oauthTokens = {
     access_token: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjdlN2JlIn1.mockOAUTH.token.eyJ1c2VybmFtZSI6InVzZXIxIiwic2NvcGUiOiJyZWFk",
     refresh_token: "mockRefreshToken1234567890abcdef",
